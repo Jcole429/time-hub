7640b50a0116e145c4e4d82f8f045a0e6386ce9a
Jcole429 <Justincole29@hotmail.com>
2021-03-25 17:10:16 -0400

    Adding basic functionality for separated projects and entries.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,29 @@
 class EntriesController < ApplicationController
+    def index
+        @entries = Entry.all().order(:date)
+    end
+
+    def new
+        @entry = Entry.new
+    end
+
+    def create
+        @entry = Entry.new(post_params)
+        puts post_params
+        if @entry.save
+            redirect_to @entry
+        else
+            render :new
+        end
+    end
+
+
+    def show
+        @entry = Entry.find(params[:id])
+    end
+
+    private
+    def post_params
+        params.require(:entry).permit(:id, :project_id, :date, :hours, :billable, :first_name, :last_name, :billable_rate)
+    end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,28 @@
 class ProjectsController < ApplicationController
   def index
-    @projects = Project.select(:project,
-    :client, 'SUM(hours) as hours', 
-    'sum(case when billable = 1 then hours else 0 end) as billable_hours',
-    'sum(hours * billable_rate) as billable_amount').group(:project, :client).order(:project)
+    @projects = Project.all().order(:project)
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(post_params)
+    puts @project
+    if @project.save
+      redirect_to @project
+    else
+      render :new
+    end
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  private
+  def post_params
+    params.require(:project).permit(:client, :project, :project_code)
   end
 end
