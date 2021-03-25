922a4097e2645297e116f1678252efde4aeafe4a
Jcole429 <Justincole29@hotmail.com>
2021-03-25 09:03:31 -0400

    Adding columns to Project model migration

--- db/migrate/20210325124927_create_projects.rb
+++ db/migrate/20210325124927_create_projects.rb
@@ -1,7 +1,15 @@
 class CreateProjects < ActiveRecord::Migration[6.1]
   def change
     create_table :projects do |t|
-
+      t.date :name
+      t.string :client
+      t.string :project
+      t.string :project_code
+      t.decimal :hours
+      t.boolean :billable
+      t.string :first_name
+      t.string :last_name
+      t.integer :billable_rate
       t.timestamps
     end
   end
