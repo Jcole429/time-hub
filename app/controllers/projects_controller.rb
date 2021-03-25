f89ca204bb5e58e98e09f7d1606b1bbe0933b2c9
Jcole429 <Justincole29@hotmail.com>
2021-03-25 14:18:39 -0400

    Updating index query. Removing helper functions. Updating index page.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,8 @@
 class ProjectsController < ApplicationController
   def index
-    @projects = Project.all
+    @projects = Project.select(:project,
+    :client, 'SUM(hours) as hours', 
+    'sum(case when billable = 1 then hours else 0 end) as billable_hours',
+    'sum(hours * billable_rate) as billable_amount').group(:project, :client).order(:project)
   end
 end
--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,22 +1,9 @@
 module ProjectsHelper
-    def get_billable_hours(project)
-        if project.billable
-            "#{project.hours} (100%)"
-        else
-            "0.00 (0%)"
-        end
-    end
 
-    def get_billable_amount(project)
-        # number_to_currency(project.billable_rate * project.hours)
-        project.billable_rate * project.hours
-    end
-
-    def get_total_billable_amount()
-        projects = Project.all
+    def get_total_billable_amount(projects)
         total = 0
         for project in projects do
-            total += get_billable_amount(project)
+            total += project.billable_amount
         end
         return total
     end
