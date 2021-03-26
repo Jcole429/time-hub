75f29dc1489f67bb92b776efe6f002a2ee364b14
Jcole429 <Justincole29@hotmail.com>
2021-03-26 12:37:17 -0400

    Adding summary controller and view

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,19 +1,2 @@
 module ProjectsHelper
-
-    def get_total_billable_amount(projects)
-        total = 0
-        for project in projects do
-            total += project.billable_amount
-        end
-        return total
-    end
-
-    def get_total_hours_tracked()
-        projects = Project.all
-        total = 0
-        for project in projects do
-            total += project.hours
-        end
-        return total
-    end
 end
