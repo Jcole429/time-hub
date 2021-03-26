71c2261c1a76cb563c8b018f12dda68c18fb6312
Jcole429 <Justincole29@hotmail.com>
2021-03-26 13:31:57 -0400

    Adding entries table to projects show view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -19,6 +19,8 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.find(params[:id])
+    @entries = Entry.all().where(project_id: @project.id)
+    puts @entries
   end
 
   private
