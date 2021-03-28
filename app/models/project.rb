2a010b5a8e1641109a1a35ae50f62747a2a3e310
Jcole429 <Justincole29@hotmail.com>
2021-03-28 13:50:42 -0400

    Showing project code and name in selection

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,8 @@
 class Project < ApplicationRecord
     has_many :entries
     validates :project, :client, :project_code, presence: true
+
+    def project_code_with_name
+        "#{project_code} - #{project}"
+    end
 end
