41b4757d1bffc3df3cd52654405d7b1327dcc638
Jcole429 <Justincole29@hotmail.com>
2021-03-29 09:06:48 -0400

    Adding Employee model and erb files.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,5 @@
 class Entry < ApplicationRecord
     belongs_to :project
+    belongs_to :employee
     validates :project_id, :date, :hours, :first_name, :last_name, :billable_rate, presence: true
 end
--- db/migrate/20210325192523_create_entries.rb
+++ db/migrate/20210325192523_create_entries.rb
@@ -2,11 +2,10 @@ class CreateEntries < ActiveRecord::Migration[6.1]
   def change
     create_table :entries do |t|
       t.belongs_to :project, foreign_key: true
+      t.belongs_to :employee, foreign_key: true
       t.date :date
       t.decimal :hours, precision: 10, scale: 2
       t.boolean :billable
-      t.string :first_name
-      t.string :last_name
       t.integer :billable_rate
       t.timestamps
     end
